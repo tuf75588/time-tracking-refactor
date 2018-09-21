7fe1a804e922959604e4f1387088ab86661f86e4
Andrew Davis <tuf75588@temple.edu>
2018-09-21 14:58:58 -0400

    add timers to timerdashboard

--- src/components/TimerForm.js
+++ src/components/TimerForm.js
@@ -5,24 +5,63 @@ class TimerForm extends Component {
     title: this.props.title || '',
     project: this.props.project || ''
   };
-
+  submitForm = () => {
+    this.props.onFormSubmit({
+      title: this.state.title,
+      project: this.state.project,
+      id: this.props.id
+    });
+  };
+  handleTitleChange = (event) =>   {
+    const val = event.target.value;
+    this.setState(() => ({
+      title: val,
+    }))
+  }
+  handleProjectChange = event => {
+    const val = event.target.value;
+    this.setState(() => ({
+      project: val,
+    }))
+  }
   render() {
-    const submitText=  this.props.id ? 'Update' : 'Create';
+    const { title, project, id } = this.props;
+    const submitText = this.props.id ? 'Update' : 'Create';
     return (
       <div className="ui centered card">
         <div className="content">
           <div className="ui form">
             <div className="field">
               <label>Title</label>
-              <input type="text" value={this.state.title} />
+              <input
+                type="text"
+                name="title"
+                value={this.state.title}
+                onChange={this.handleTitleChange}
+              />
             </div>
             <div className="field">
               <label>Project</label>
-              <input type="text" value={this.state.project} />
+              <input
+                type="text"
+                name="project"
+                value={this.state.project}
+                onChange={this.handleProjectChange}
+              />
             </div>
             <div className="ui two bottom attached buttons">
-              <button className="ui basic blue button">{submitText}</button>
-              <button className="ui basic red button" onClick={this.props.onClose}>Cancel</button>
+              <button
+                className="ui basic blue button"
+                onClick={this.submitForm}
+              >
+                {submitText}
+              </button>
+              <button
+                className="ui basic red button"
+                onClick={this.props.onClose}
+              >
+                Cancel
+              </button>
             </div>
           </div>
         </div>
